c8a90fd2566a679c784291b6d1314bd60b29534e
dimanu-py <diegomtz126@gmail.com>
2024-03-21 17:00:24 +0100

    feat(mars-rover): added logic to handle end of the grid movement

--- simple_mars_rover/src/mars_rover.py
+++ simple_mars_rover/src/mars_rover.py
@@ -4,4 +4,6 @@ class Rover:
 
     def execute(self, command: str) -> str:
         vertical_position = len(command)
+        if vertical_position >= 10:
+            vertical_position = 0
         return f"{vertical_position}:0:N"
