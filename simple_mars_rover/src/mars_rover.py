2c6d1f06fed9fd952369b529a73c400a209cc2be
dimanu-py <diegomtz126@gmail.com>
2024-03-21 17:09:17 +0100

    refactor(mars-rover): add 25 moves forward

--- simple_mars_rover/src/mars_rover.py
+++ simple_mars_rover/src/mars_rover.py
@@ -4,7 +4,6 @@ GRID_SIZE = 10
 class Rover:
 
     def execute(self, command: str) -> str:
-        vertical_position = len(command)
-        if vertical_position >= GRID_SIZE:
-            vertical_position -= GRID_SIZE
+        vertical_position = len(command) % GRID_SIZE
+
         return f"{vertical_position}:0:N"
