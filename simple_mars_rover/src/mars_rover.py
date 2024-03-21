56c32fcc739eacd08ebd82ee57964ec6dcec5d87
dimanu-py <diegomtz126@gmail.com>
2024-03-21 17:01:24 +0100

    refactor(mars-rover): extracted grid size to constant

--- simple_mars_rover/src/mars_rover.py
+++ simple_mars_rover/src/mars_rover.py
@@ -1,9 +1,10 @@
+GRID_SIZE = 10
 
 
 class Rover:
 
     def execute(self, command: str) -> str:
         vertical_position = len(command)
-        if vertical_position >= 10:
+        if vertical_position >= GRID_SIZE:
             vertical_position = 0
         return f"{vertical_position}:0:N"
