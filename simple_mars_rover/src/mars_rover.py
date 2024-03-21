725ac687c5c2a71ef093e1916d1373a892c1080d
dimanu-py <diegomtz126@gmail.com>
2024-03-21 17:33:28 +0100

    refactor(mars-rover): execute method refactored to deal with R and M

--- simple_mars_rover/src/mars_rover.py
+++ simple_mars_rover/src/mars_rover.py
@@ -1,15 +1,11 @@
 GRID_SIZE = 10
+ORIENTATION = {0: 'N', 1: 'E', 2: 'S', 3: 'W'}
 
 
 class Rover:
 
     def execute(self, command: str) -> str:
-        if command == "R":
-            return "0:0:E"
-        elif command == "RR":
-            return "0:0:S"
-        elif command == "RRR":
-            return "0:0:W"
-        vertical_position = len(command) % GRID_SIZE
-        return f"{vertical_position}:0:N"
-    
+
+        number_rigth_rotations = command.count('R') % 4
+        vertical_position = command.count('M') % GRID_SIZE
+        return f"{vertical_position}:0:{ORIENTATION[number_rigth_rotations]}"
