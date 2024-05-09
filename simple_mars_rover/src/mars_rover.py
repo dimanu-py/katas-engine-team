de3d9a090f4e7601a77b093773a1056d9c0429c1
dimanu-py <diegomtz126@gmail.com>
2024-05-09 16:49:22 +0200

    refactor(mars-rover): behavior of moving in vertical direction

--- simple_mars_rover/src/mars_rover.py
+++ simple_mars_rover/src/mars_rover.py
@@ -6,15 +6,19 @@ class Rover:
 
     def __init__(self) -> None:
         self.orientation = 0
-        self.horizontal_position = 0
         self.vertical_position = 0
 
     def execute(self, command: str) -> str:
         for c in command:
             if c == "R":
                 self.orientation += 1
-            if c == "L":
+            elif c == "L":
                 self.orientation -= 1
+            elif c == "M":
+                if self.orientation == 0:
+                    self.vertical_position += 1
+                elif self.orientation == 2:
+                    self.vertical_position -= 1
 
-        self.vertical_position = command.count('M') % GRID_SIZE
+        self.vertical_position %= GRID_SIZE
         return f"{self.vertical_position}:0:{ORIENTATION[self.orientation % 4]}"
