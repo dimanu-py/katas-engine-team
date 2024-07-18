d04af237501376dd6b63a4b7e09780a67b842a1c
dimanu-py <diegomtz126@gmail.com>
2024-07-18 17:18:59 +0200

    style(tennis-4): follow python convention for names

--- tennis_refactoring/src/tennis_4.py
+++ tennis_refactoring/src/tennis_4.py
@@ -1,58 +1,57 @@
 class TennisGame4:
     scores = ["Love", "Fifteen", "Thirty", "Forty"]
 
-    def __init__(self, player1Name, player2Name):
-        self.server = player1Name
-        self.receiver = player2Name
-        self.serverScore = 0
-        self.receiverScore = 0
+    def __init__(self, player_one_name, player_two_name):
+        self.server = player_one_name
+        self.receiver = player_two_name
+        self.server_score = 0
+        self.receiver_score = 0
 
-    def won_point(self, playerName):
-        if self.server == playerName:
-            self.serverScore += 1
+    def won_point(self, player_name):
+        if self.server == player_name:
+            self.server_score += 1
         else:
-            self.receiverScore += 1
+            self.receiver_score += 1
 
     def score(self):
-        if self.isDeuce():
+        if self.is_deuce():
             result = TennisResult("Deuce", "")
-        elif self.serverHasWon():
+        elif self.server_has_won():
             result = TennisResult("Win for " + self.server, "")
-        elif self.receiverHasWon():
+        elif self.receiver_has_won():
             result = TennisResult("Win for " + self.receiver, "")
-        elif self.serverHasAdvantage():
+        elif self.server_has_advantage():
             result = TennisResult("Advantage " + self.server, "")
-        elif self.receiverHasAdvantage():
+        elif self.receiver_has_advantage():
             result = TennisResult("Advantage " + self.receiver, "")
         else:
-            result = TennisResult(self.scores[self.serverScore], self.scores[self.receiverScore])
+            result = TennisResult(self.scores[self.server_score], self.scores[self.receiver_score])
         return result.format()
 
-    def receiverHasAdvantage(self):
-        return self.receiverScore >= 4 and (self.receiverScore - self.serverScore) == 1
+    def receiver_has_advantage(self):
+        return self.receiver_score >= 4 and (self.receiver_score - self.server_score) == 1
 
-    def serverHasAdvantage(self):
-        return self.serverScore >= 4 and (self.serverScore - self.receiverScore) == 1
+    def server_has_advantage(self):
+        return self.server_score >= 4 and (self.server_score - self.receiver_score) == 1
 
-    def receiverHasWon(self):
-        return self.receiverScore >= 4 and (self.receiverScore - self.serverScore) >= 2
+    def receiver_has_won(self):
+        return self.receiver_score >= 4 and (self.receiver_score - self.server_score) >= 2
 
-    def serverHasWon(self):
-        return self.serverScore >= 4 and (self.serverScore - self.receiverScore) >= 2
+    def server_has_won(self):
+        return self.server_score >= 4 and (self.server_score - self.receiver_score) >= 2
 
-    def isDeuce(self):
-        return self.serverScore >= 3 and self.receiverScore >= 3 and (self.serverScore == self.receiverScore)
+    def is_deuce(self):
+        return self.server_score >= 3 and self.receiver_score >= 3 and (self.server_score == self.receiver_score)
 
 
 class TennisResult:
-    def __init__(self, serverScore, receiverScore):
-        self.serverScore = serverScore
-        self.receiverScore = receiverScore
+    def __init__(self, server_score, receiver_score):
+        self.server_score = server_score
+        self.receiver_score = receiver_score
 
     def format(self):
-        if "" == self.receiverScore:
-            return self.serverScore
-        if self.serverScore == self.receiverScore:
-            return self.serverScore + "-All"
-        return self.serverScore + "-" + self.receiverScore
-
+        if "" == self.receiver_score:
+            return self.server_score
+        if self.server_score == self.receiver_score:
+            return self.server_score + "-All"
+        return self.server_score + "-" + self.receiver_score
