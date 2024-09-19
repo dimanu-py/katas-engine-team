8c77bbb8cf211fc1e1346d952ee39c7f19deb73c
Pedro_frenetic <pedro.sanchez@frenetic.ai>
2024-09-19 16:33:10 +0200

    tests(test-service): add test to validate logic for simple friendless user

--- trip_service/tests/test_service.py
+++ trip_service/tests/test_service.py
@@ -1,4 +1,5 @@
 import pytest
+from typing import Union
 
 from trip_service.src.exceptions import UserNotLoggedInException
 from trip_service.src.service import TripService
@@ -7,15 +8,25 @@ from trip_service.src.user import User
 
 class SeamTripService(TripService):
 
+    def __init__(self, user: Union[User, None]):
+        self.user = user
+
     def get_logged_user(self) -> User:
-        return None
+        return self.user
 
 
 class TestTripService:
 
     def test_user_cannot_get_trips_if_not_logged_in(self) -> None:
         not_logged_user = User()
-        trip_service = SeamTripService()
+        trip_service = SeamTripService(user=None)
 
         with pytest.raises(UserNotLoggedInException):
             trip_service.get_trips_by_user(not_logged_user)
+
+    def test_user_without_friends_returns_empty_trip_list(self) -> None:
+        friendless_user = User()
+        trip_service = SeamTripService(user=friendless_user)
+
+        trips = trip_service.get_trips_by_user(friendless_user)
+        assert len(trips) == 0
